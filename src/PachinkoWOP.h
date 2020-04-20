d29d35050abaedffb91a865e76c30f81c65ca668
Michael Cooper <mc454916@ohio.edu>
2020-04-20 15:15:14 -0400

    Update Functions with new Enum

--- src/PachinkoWOP.h
+++ src/PachinkoWOP.h
@@ -9,13 +9,19 @@ class PachinkoWOP : public Aftr::WO
 {
 public:
 	//WOMacroDeclaration(PachinkoWOP, WO);
-	static PachinkoWOP* New(physx::PxPhysics* p, physx::PxScene* s, const std::string& path, Aftr::Vector scale, Aftr::MESH_SHADING_TYPE mst, bool stat, physx::PxVec3 pos);
+	// enum to pass to PackingoWOP::New allowing the user to create a specific type of WOP;
+	enum PxObj { Floor, Ball, Peg };
+	static PachinkoWOP* New(physx::PxPhysics* p, physx::PxScene* s, const std::string& path, Aftr::Vector scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3 pos);
 	static PxActor* getPxActor(physx::PxPhysics* p);
+	//static PachinkoWOP* createPeg();
+	//static void __setPx(physx::PxPhysics* p, physx::PxScene* s);
 	virtual void setPosition(Aftr::Vector v);
+	void setRotation(const float* info);
 	void updatePoseFromPhysicsEngine(physx::PxActor* actor);
+
 protected:
 	PachinkoWOP(physx::PxPhysics* p, physx::PxScene* s);
-	void onCreate(const std::string& path, const Aftr::Vector& scale, Aftr::MESH_SHADING_TYPE mst, bool stat, physx::PxVec3& pos);
+	void onCreate(const std::string& path, const Aftr::Vector& scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3& pos);
 
 	physx::PxPhysics* p;
 	physx::PxScene* s;
