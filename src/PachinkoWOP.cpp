ceca5700285a4d9f6529e07ce3cd34bde1e8c741
Michael Cooper <mc454916@ohio.edu>
2020-04-25 13:51:01 -0400

    Updated to include initial rotations of objects
    I hate quaternions

--- src/PachinkoWOP.cpp
+++ src/PachinkoWOP.cpp
@@ -1,6 +1,8 @@
 #include <PachinkoWOP.h>
 #include "PxPhysicsAPI.h"
 #include "PxRigidStatic.h"
+#include "PxRigidDynamic.h"
+#include "PxRigidActor.h"
 #include "PxMaterial.h"
 #include "PxActor.h"
 #include "PxRigidStatic.h"
@@ -15,10 +17,10 @@ using physx::PxRigidActor;
 using physx::PxRigidStatic;*/
 using namespace physx;
 
-PachinkoWOP* PachinkoWOP::New(physx::PxPhysics* p, physx::PxScene* s, const std::string& path, Aftr::Vector scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3 pos)
+PachinkoWOP* PachinkoWOP::New(physx::PxPhysics* p, physx::PxScene* s, const std::string& path, Aftr::Vector scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3 pos, physx::PxQuat& rot)
 {
 	PachinkoWOP* wo = new PachinkoWOP(p, s);
-	wo->onCreate(path, scale, mst, ty, pos);
+	wo->onCreate(path, scale, mst, ty, pos, rot);
 	return wo;
 }
 
@@ -29,43 +31,59 @@ PachinkoWOP::PachinkoWOP(physx::PxPhysics* p, physx::PxScene* s) : IFace(this), 
 	this->a = nullptr;
 }
 
-void PachinkoWOP::onCreate(const std::string& path, const Aftr::Vector& scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3& pos)
+void PachinkoWOP::onCreate(const std::string& path, const Aftr::Vector& scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3& pos, physx::PxQuat& rot)
 {
 	WO::onCreate(path, scale, mst);
 	PxMaterial* gMaterial = p->createMaterial(0.9f, 0.9f, 0.3f);
-	PxShape* shape;
-	PxTransform t(pos);
-
+	PxTransform t(pos, rot);
+	this->t = ty;
 	switch (ty)
 	{
 	case Ball: 
-			shape = p->createShape(PxSphereGeometry(1), *gMaterial, true);
-			a = p->createRigidDynamic(t);
-			break;
+	{
+		shape = p->createShape(PxSphereGeometry(1), *gMaterial, true);
+		a = p->createRigidDynamic(t);
+		break;
+	}
 	case Peg:	// create box with contact and rest offsets to create a "lozenge" shape 
 				// https://github.com/NVIDIAGameWorks/PhysX/issues/50#issuecomment-458509791
 				// just use capsules for now, can't get the example to work
-			shape = p->createShape(PxCapsuleGeometry(1, 2), *gMaterial, true);
-			this->rotateAboutGlobalY(Aftr::DEGtoRAD * 90);
-			//shape->setContactOffset(3.0f);
-			//shape->setRestOffset(2.0f);
-			a = p->createRigidStatic(t);
-			break;
-	case Floor: 
-			shape = p->createShape(PxBoxGeometry(100, 100, 1), *gMaterial, true);
-			a = p->createRigidStatic(t);
-			break;
+	{
+		shape = p->createShape(PxCapsuleGeometry(1, 2), *gMaterial, true);
+
+		//shape->setContactOffset(3.0f);
+		//shape->setRestOffset(2.0f);
+		//PxRigidDynamic* rb = p->createRigidDynamic(t);
+		//rb->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
+		//rb->setKinematicTarget(PxTransform(pos, PxQuat(rot.x, rot.y, rot.z, 0)));
+		a = p->createRigidStatic(t);//rb;
+		break;
+	}
+	case Plane: 
+	{
+		shape = p->createShape(PxBoxGeometry(100, 100, 1), *gMaterial, true);
+		a = p->createRigidStatic(t);
+		break;
+	}
 	default: 
-			std::cerr << "Default case in PachinkoWOP OnCreate!" << std::endl;
-			shape = p->createShape(PxBoxGeometry(1, 1, 1), *gMaterial, true);
-			a = p->createRigidStatic(t);
-			break;
+	{
+		std::cerr << "Default case in PachinkoWOP OnCreate!" << std::endl;
+		shape = p->createShape(PxBoxGeometry(1, 1, 1), *gMaterial, true);
+		a = p->createRigidStatic(t);
+		break;
+	}
 	}
 	//shape->setContactOffset(0.01f);
 
+	// testing rotation
 	a->attachShape(*shape);
 	a->userData = this;
 	this->s->addActor(*a);
+
+	//initial physics update (runs for static actors too)
+	PachinkoWOP* wo = static_cast<PachinkoWOP*>(a->userData);
+	wo->updatePoseFromPhysicsEngine(a);
+
 }
 
 PxActor* PachinkoWOP::getPxActor(physx::PxPhysics* p)
@@ -100,6 +118,10 @@ void PachinkoWOP::updatePoseFromPhysicsEngine(physx::PxActor* actor)
 	}
 
 	setRotation(info);
+	if (this->t == Peg)
+	{
+		this->rotateAboutGlobalY(Aftr::DEGtoRAD * 90);
+	}
 }
 
 void PachinkoWOP::setPosition(Aftr::Vector v)
--- src/PachinkoWOP.h
+++ src/PachinkoWOP.h
@@ -10,22 +10,23 @@ class PachinkoWOP : public Aftr::WO
 public:
 	//WOMacroDeclaration(PachinkoWOP, WO);
 	// enum to pass to PackingoWOP::New allowing the user to create a specific type of WOP;
-	enum PxObj { Floor, Ball, Peg };
-	static PachinkoWOP* New(physx::PxPhysics* p, physx::PxScene* s, const std::string& path, Aftr::Vector scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3 pos);
+	enum PxObj { Plane, Ball, Peg };
+	static PachinkoWOP* New(physx::PxPhysics* p, physx::PxScene* s, const std::string& path, Aftr::Vector scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3 pos, physx::PxQuat& rot);
 	static PxActor* getPxActor(physx::PxPhysics* p);
 	//static PachinkoWOP* createPeg();
 	//static void __setPx(physx::PxPhysics* p, physx::PxScene* s);
 	virtual void setPosition(Aftr::Vector v);
 	void setRotation(const float* info);
 	void updatePoseFromPhysicsEngine(physx::PxActor* actor);
-
+	PxObj t;
 protected:
 	PachinkoWOP(physx::PxPhysics* p, physx::PxScene* s);
-	void onCreate(const std::string& path, const Aftr::Vector& scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3& pos);
-
+	void onCreate(const std::string& path, const Aftr::Vector& scale, Aftr::MESH_SHADING_TYPE mst, PxObj ty, physx::PxVec3& pos, physx::PxQuat& rot);
+private:
 	physx::PxPhysics* p;
 	physx::PxScene* s;
 	physx::PxRigidActor* a;
+	physx::PxShape* shape;
 	
 	//PachinkoWOP* t = this;
 };
